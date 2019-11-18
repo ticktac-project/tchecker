5ab0ce794c505e7aeaac5d82e664ff164a782aff
Gerald Point <gerald.point@labri.fr>
2019-11-18 16:06:52 +0100

    chg: new tests for 16 bits and 64 bits DBMs

---
+++ examples/ad94_Long.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This file is a part of the TChecker project.
+#
+# See files AUTHORS and LICENSE for copyright details.
+
+echo "# labels=green"
+
+cat "$(dirname $0)/ad94_Long.txt"
+
