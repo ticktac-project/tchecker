47c2f596ff2f8b3368d7aec64827a62ac508cdb5
Gerald Point <gerald.point@labri.fr>
2019-12-02 14:49:50 +0100

    chg: update test suites w.r.t. ite and land

--- test/simple-nr/bubble-sort.sh
+++ test/simple-nr/bubble-sort.sh
@@ -5,16 +5,18 @@
 SEED=12345
 MAX=1000
 N=10
+ML_DO=no
+S_AND=no
 
 usage() {
     cat >&2 << EOF
-usage: $0 [-s seed] [-n array-size] [-m max-value] [-h]
-Default values are: -s ${SEED} -n ${N} -m ${MAX}
+usage: $0 [-s seed] [-n array-size] [-m max-value] [--multilines-do] [--sequential-and] [-h]
+Current values are: -s ${SEED} -n ${N} -m ${MAX}
 Use -h for this help message.
 EOF
 }
 
-TEMP=$(getopt -o "s:m:n:h" -- "$@")
+TEMP=$(getopt -o "s:m:n:h" -l "sequential-and,multilines-do" -- "$@")
 if test $? != 0; then
     usage $0
     exit 1
@@ -23,15 +25,25 @@ fi
 eval set -- "${TEMP}"
 
 while true ; do
+        shifted=1
         case "$1" in
-                -s) SEED=$2; shift 2 ;;
-                -n) N=$2; shift 2 ;;
-                -m) MAX=$2; shift 2 ;;
+                -s) SEED=$2; shifted=2 ;;
+                -n) N=$2; shifted=2 ;;
+                -m) MAX=$2; shifted=2 ;;
                 -h) usage $0; exit 0;;
+                --multilines-do) ML_DO=yes ;;
+                --sequential-and) S_AND=yes ;;
                 --) shift; break ;;
                 *) echo "Internal error on argument '$1' !" ; exit 1 ;;
         esac
+        shift ${shifted}
 done
+
+if test $# != 0; then
+    echo "wrong # arguments" >&2
+    usage $0
+    exit 1
+fi
 
 init_array() {
     RANDOM=$1
@@ -55,6 +67,11 @@ init_array() {
 #   events is a colon-separated list of process@event
 
 cat << EOF
+# File generated with the following setting:
+# N = ${N} SEED = ${SEED} MAX = ${MAX}
+# Multi-lines do = ${ML_DO}
+# Sequential and = ${S_AND}
+
 system: S
 int:1:${N}:${N}:${N}:n
 int:${N}:0:${MAX}:0:A
@@ -72,8 +89,58 @@ event: initA
 event: sortA
 event: checkA
 edge:P:init:A:initA{do:$(init_array ${SEED}) }
+EOF
+
+if test "${ML_DO}" = "yes"; then
+    cat << EOF
+edge:P:A:B:sortA{do:
+while (i < n - 1) do
+    j = 0;
+    while (j < n-i-1) do
+        if (A[j] > A[j+1]) then
+            tmp = A[j];
+            A[j] = A[j+1];
+            A[j+1] = tmp;
+            tmp = 0
+        endif ;
+        j = j + 1
+     done;
+     i = i + 1
+done;
+i = 0
+}
+EOF
+    if  test "${S_AND}" = "yes"; then
+        cat << EOF
+edge:P:B:B:checkA{provided: i < n-1 && A[i] <= A[i+1] : do : i = i+1 }
+edge:P:B:ERR:checkA{provided: i < n-1 && A[i] > A[i+1] }
+EOF
+    else
+        cat << EOF
+edge:P:B:B:checkA{provided: i < n-1 : do :
+if A[i] <= A[i+1] then
+    i = i+1
+else
+    error = 1
+endif
+}
+edge:P:B:ERR:checkA{provided: error }
+EOF
+    fi
+else
+    cat << EOF
 edge:P:A:B:sortA{do: while (i < n - 1) do j = 0; while (j < n-i-1) do if (A[j] > A[j+1]) then tmp = A[j]; A[j] = A[j+1]; A[j+1] = tmp; tmp = 0 endif ; j = j + 1 done; i = i + 1 done; i = 0 }
+EOF
+    if  test "${S_AND}" = "yes"; then
+        cat << EOF
+edge:P:B:B:checkA{provided: i < n-1 && A[i] <= A[i+1] : do : i = i+1 }
+edge:P:B:ERR:checkA{provided: i < n-1 && A[i] > A[i+1] }
+EOF
+    else
+        cat << EOF
 edge:P:B:B:checkA{provided: i < n-1 : do : if A[i] <= A[i+1] then i = i+1 else error = 1 endif }
 edge:P:B:ERR:checkA{provided: error }
 EOF
+    fi
+fi
 
