8ab33ebaa8aff8865ec42db7caf6250c49540d7d
Gerald Point <gerald.point@labri.fr>
2020-10-13 16:59:06 +0200

    chg: enforce Catch2 branch

--- ci-scripts/install-sources.sh
+++ ci-scripts/install-sources.sh
@@ -4,13 +4,14 @@ set -eu
 
 echo "#### INSTALLING Catch2"
 
-git clone https://github.com/catchorg/Catch2.git ${CATCH2_REPO} || true
+git clone --branch ${CATCH2_BRANCH} https://github.com/catchorg/Catch2.git ${CATCH2_REPO} || true
 mkdir -p ${CATCH2_REPO}/build || true
 
 pushd ${CATCH2_REPO}/build
  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ..
  make -j ${NB_CPUS}
  make install
+ ls -R ${INSTALL_DIR}
 popd
 
 
