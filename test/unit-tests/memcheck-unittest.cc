b788491247b3d34ef72d0a250fa452255a301e76
Gerald Point <gerald.point@labri.fr>
2021-07-20 13:34:52 +0200

    fix: fix unittest wrapper used to invoke valgrind

--- test/unit-tests/memcheck-unittest.cc
+++ test/unit-tests/memcheck-unittest.cc
@@ -7,14 +7,19 @@
 #include "memcheck-unittest.hh"
 #include <cstdlib>
 #include <sstream>
+#include <iostream>
 
 int main(int argc, char ** argv)
 {
   std::ostringstream oss;
 
   oss << VALGRIND_PROGRAM << " " << VALGRIND_OPTIONS << " " << TESTED_EXECUTABLE;
-  for (int i = 1; i < argc; i++)
-    oss << " " << argv[i];
-
-  return system(oss.str().c_str());
+  if (argc > 1) {
+    oss << " \"" << argv[1];
+    for (int i = 2; i < argc; i++)
+      oss << " " << argv[i];
+    oss << "\"";
+  }
+  int val = system(oss.str().c_str());
+  return (val ? 1 : 0);
 }
