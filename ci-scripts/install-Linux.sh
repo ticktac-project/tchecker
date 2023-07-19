4b9ca3d57a6a204dba52b80121a47a142e6f1044
Frédéric Herbreteau <fh@labri.fr>
2023-07-19 14:07:43 +0200

    Fixing GitHub CI/CD (libboost 1.81 required)

--- ci-scripts/install-Linux.sh
+++ ci-scripts/install-Linux.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+LIBBOOST_VERSION=1.81
+
 set -eu
 
 sudo apt-get update
-sudo apt-get install libboost-all-dev valgrind
+sudo apt-get install valgrind \
+    libboost${LIBBOOST_VERSION}-dev  \
+    libboost-json${LIBBOOST_VERSION}-dev
 
 $(dirname $0)/install-sources.sh
-
-
