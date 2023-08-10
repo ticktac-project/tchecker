a755edf64de4c69732014f9d28dab0cbbc3ab3c5
Frédéric Herbreteau <fh@labri.fr>
2023-08-10 09:18:27 +0200

    Added string representation of bit sets

--- include/tchecker/utils/string.hh
+++ include/tchecker/utils/string.hh
@@ -8,8 +8,11 @@
 #ifndef TCHECKER_STRING_HH
 #define TCHECKER_STRING_HH
 
+#include <functional>
 #include <string>
 #include <vector>
+
+#include <boost/dynamic_bitset.hpp>
 
 /*!
  \file string.hh
@@ -25,6 +28,15 @@ namespace tchecker {
  \return vector of substrings in s delimited by d
 */
 std::vector<std::string> split(std::string const & s, char d);
+
+/*!
+ \brief String representation of a bitset
+ \param bitset : a bit set
+ \param name : map identifiers to names
+ \return string representation of bitset with element names from name
+ \throw may throw if name throws
+ */
+std::string to_string(boost::dynamic_bitset<> const & bitset, std::function<std::string(std::size_t)> name);
 
 } // namespace tchecker
 
--- src/utils/string.cc
+++ src/utils/string.cc
@@ -11,12 +11,6 @@
 
 namespace tchecker {
 
-/*!
- \brief Split a string into a vector of strings
- \param s : a string
- \param d : a delimiter
- \return vector of substrings in s delimited by d
-*/
 std::vector<std::string> split(std::string const & s, char d)
 {
   std::stringstream sstream(s);
@@ -25,6 +19,20 @@ std::vector<std::string> split(std::string const & s, char d)
   while (std::getline(sstream, substr, d))
     v.push_back(substr);
   return v;
+}
+
+std::string to_string(boost::dynamic_bitset<> const & bitset, std::function<std::string(std::size_t)> name)
+{
+  std::stringstream ss;
+  ss << "{";
+  auto first = bitset.find_first();
+  for (auto i = first; i != bitset.npos; i = bitset.find_next(i)) {
+    if (i != first)
+      ss << ",";
+    ss << name(i);
+  }
+  ss << "}";
+  return ss.str();
 }
 
 } // namespace tchecker
