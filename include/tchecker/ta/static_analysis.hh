f9bd9df43898d42167c315c39c3f89cb3a14db1f
Frédéric Herbreteau <fh@labri.fr>
2023-11-01 11:24:08 +0100

    Static analysis on systems (diagonal free, etc)

--- include/tchecker/ta/static_analysis.hh
+++ include/tchecker/ta/static_analysis.hh
@@ -25,6 +25,34 @@ namespace ta {
  */
 bool has_guarded_weakly_synchronized_event(tchecker::ta::system_t const & system);
 
+/*!
+ \brief Checks if a system has diagonal clock constraints
+ \param system : a system of timed processes
+ \return true if system has a diagonal constraint x - y # c in guard or a location invariant,
+ false otherwise
+ */
+bool has_diagonal_constraint(tchecker::ta::system_t const & system);
+
+/*!
+ \brief Checks if a system has a non-constant clock reset
+ \param system : a system of timed processes
+ \return true if system has a non-constant reset x := y or x := y + c in the statement of
+ a transition, false otherwise
+ */
+bool has_non_constant_reset(tchecker::ta::system_t const & system);
+
+/*!
+ \brief Checks if a system has a shared clock
+ \param system : a system of timed processes
+ \return true if there exists a clock x that is used by more than one process,
+ false otherwise
+ \note clock arrays may lead to false positive answer. For instance, if one process uses
+ x[i] and another process uses x[j], where i or j cannot be determined, then the analysis
+ will conclude that x is shared by the two processes, although it may not be the case as
+ i != j (which cannot be determined statically)
+ */
+bool has_shared_clocks(tchecker::ta::system_t const & system);
+
 } // end of namespace ta
 
 } // end of namespace tchecker
--- src/ta/static_analysis.cc
+++ src/ta/static_analysis.cc
@@ -6,9 +6,11 @@
  */
 
 #include "tchecker/expression/static_analysis.hh"
+#include "tchecker/statement/static_analysis.hh"
 #include "tchecker/system/static_analysis.hh"
 #include "tchecker/ta/static_analysis.hh"
 #include "tchecker/ta/system.hh"
+#include "tchecker/variables/static_analysis.hh"
 
 namespace tchecker {
 
@@ -30,6 +32,43 @@ bool has_guarded_weakly_synchronized_event(tchecker::ta::system_t const & system
     }
   }
   return false;
+}
+
+bool has_diagonal_constraint(tchecker::ta::system_t const & system)
+{
+  // Check location invariants
+  for (tchecker::system::loc_const_shared_ptr_t const & loc : system.locations()) {
+    tchecker::has_clock_constraints_t invariant_has_clock_constraints =
+        tchecker::has_clock_constraints(system.invariant(loc->id()));
+    if (invariant_has_clock_constraints.diagonal)
+      return true;
+  }
+
+  // Check edge guards
+  for (tchecker::system::edge_const_shared_ptr_t const & edge : system.edges()) {
+    tchecker::has_clock_constraints_t guard_has_clock_constrains = tchecker::has_clock_constraints(system.guard(edge->id()));
+    if (guard_has_clock_constrains.diagonal)
+      return true;
+  }
+
+  return false;
+}
+
+bool has_non_constant_reset(tchecker::ta::system_t const & system)
+{
+  // Check edge statements
+  for (tchecker::system::edge_const_shared_ptr_t const & edge : system.edges()) {
+    tchecker::has_clock_resets_t statement_has_clock_resets = tchecker::has_clock_resets(system.statement(edge->id()));
+    if (statement_has_clock_resets.clock || statement_has_clock_resets.sum)
+      return true;
+  }
+  return false;
+}
+
+bool has_shared_clocks(tchecker::ta::system_t const & system)
+{
+  tchecker::variable_access_map_t access_map = tchecker::variable_access(system);
+  return access_map.has_shared_variable(tchecker::VTYPE_CLOCK);
 }
 
 } // end of namespace ta
