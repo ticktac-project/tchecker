3fcc48c7197470e8541c66b49b089fcacd58fbdb
Gerald Point <gerald.point@labri.fr>
2023-11-21 16:53:15 +0100

    do not install the last Boost version if not available

--- ci-scripts/install-Linux.sh
+++ ci-scripts/install-Linux.sh
@@ -4,9 +4,11 @@ LIBBOOST_VERSION=1.81
 
 set -eu
 
-sudo apt-get update
-sudo apt-get install valgrind \
-    libboost${LIBBOOST_VERSION}-dev  \
-    libboost-json${LIBBOOST_VERSION}-dev
+apt-get update
+apt-get install -y valgrind
+if ! apt-get install -y libboost${LIBBOOST_VERSION}-dev  \
+     libboost-json${LIBBOOST_VERSION}-dev; then
+  apt-get install -y libboost-dev
+fi
 
 $(dirname $0)/install-sources.sh
