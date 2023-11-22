5aae89295efa59fa343d2f975fb83e8fa73d4908
Gerald Point <gerald.point@labri.fr>
2023-11-22 10:08:22 +0100

    update Catch2 installation and move to version 3.4.0

--- ci-scripts/install-sources.sh
+++ ci-scripts/install-sources.sh
@@ -7,12 +7,13 @@ echo "#### INSTALLING Catch2"
 git clone --branch ${CATCH2_BRANCH} ${CATCH2_REPO_URL} ${CATCH2_DIRECTORY} || true
 mkdir -p ${CATCH2_DIRECTORY}/build || true
 
-pushd ${CATCH2_DIRECTORY}/build
+HERE=${PWD}
+cd ${CATCH2_DIRECTORY}/build
  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_CXX_COMPILER=${CXX} ..
  make -j ${NB_CPUS}
  make install
  ls -R ${INSTALL_DIR}
-popd
+cd ${HERE}
 
 
 
