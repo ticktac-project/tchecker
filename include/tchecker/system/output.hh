98db37fd092b97c9088940ec1d204ffc986727eb
Frédéric Herbreteau <fh@labri.fr>
2025-03-10 14:05:15 +0100

    Added output option

--- include/tchecker/system/output.hh
+++ include/tchecker/system/output.hh
@@ -35,15 +35,25 @@ namespace system {
 void output_tck(std::ostream & os, tchecker::system::system_t const & s);
 
 /*!
+ \brief Type of graphviz output
+*/
+enum graphviz_output_t {
+  GRAPHVIZ_FULL = 0,      // Full output
+  GRAPHVIZ_ONLY_PROCESSES // Only output processes
+};
+
+/*!
  \brief Output a system following the dot graphviz syntax
  \param os : output stream
  \param s : system
  \param delimiter : separator between process name and location name for node
  names
- \post s has been output to os following the dot graphviz syntax
+ \param output_type : type of graphviz output
+ \post s has been output to os following the dot graphviz syntax according to output_type
  \throw std::runtime_error : if a location or an edge in s has an attribute "label"
 */
-void output_dot(std::ostream & os, tchecker::system::system_t const & s, std::string const & delimiter);
+void output_dot(std::ostream & os, tchecker::system::system_t const & s, std::string const & delimiter,
+                tchecker::system::graphviz_output_t output_type);
 
 /*!
  \brief Output a system following the JSON syntax
